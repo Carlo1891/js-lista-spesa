dd8992609d38dcf098606809480d643590f4a7e9
Carlo1891 <carlo.18@live.it>
2022-06-21 15:30:57 +0200

    Inizio

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Consegna:
+// Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+// Consigli:
+// Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+// Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
